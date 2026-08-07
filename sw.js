cfb067330905b8c9e6147024275a6b3556aa61a6
ansarinafeesa28-art <ansarinafeesa28@gmail.com>
2026-08-07 07:35:08 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,5 @@
 self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Installed');
   self.skipWaiting();
-});
-
-self.addEventListener('activate', (e) => {
-  console.log('[Service Worker] Activated');
 });
 
 self.addEventListener('fetch', (e) => {
